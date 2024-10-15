2f5339f3ad5cd2f4fcbbf7795897539213a7c193
Vhickyzambi <vitoriasilva55032@gmail.com>
2024-10-14 22:49:36 -0300

    add: finalistico

--- Agendamento/agendamento.js
+++ Agendamento/agendamento.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (error) throw error;
 
                 alert('Deslogado com sucesso!');
-                window.location.href = '../HOME/index.html';
+                window.location.href = '../index.html';
             } catch (error) {
                 console.error('Erro ao deslogar:', error.message);
                 alert('Erro ao deslogar: ' + error.message);
